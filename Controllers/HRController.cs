f8814c95a502bc649e2601b9b02a845e87003ee6
Lwazi-Mesatywa <st10092086@vcconnect.edu.za>
2024-11-23 00:06:59 +0200

    Creation of HR table and updating of the View to recieve all the accepted Claims. This is to ensure the HR index prints out reciepts for the acceepted claims only

--- Controllers/HRController.cs
+++ Controllers/HRController.cs
@@ -4,9 +4,16 @@ namespace Lecture_Claims_System_Web_Application.Controllers
 {
     public class HRController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            // Get all claims with the status of "Accepted"
+            var acceptedClaims = ClaimRepository.Instance.AllClaims
+                .Where(c => c.Status == "Accepted")
+                .ToList();
+
+            // Return the view with the accepted claims
+            return View(acceptedClaims);
         }
     }
 }
