114a1c1ff26798b1271636cff9e1bf28d2c49857
Lwazi-Mesatywa <st10092086@vcconnect.edu.za>
2024-11-23 00:26:50 +0200

    Added the HR Report feature that is used to generate a reportt using Crystal engine that shows all the approved claims that the lecturers submitted which met the minimum requirements.

--- Controllers/HRController.cs
+++ Controllers/HRController.cs
@@ -1,9 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CrystalDecisions.CrystalReports.Engine;
+using Grpc.Core;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Lecture_Claims_System_Web_Application.Controllers
 {
     public class HRController : Controller
     {
+        public IActionResult Report()
+        {
+            // Load the report
+            ReportDocument reportDocument = new ReportDocument();
+            reportDocument.Load(Server.MapPath("~/Reports/ClaimReport.rpt"));
+
+            //  pass data from your claim repository to the report
+            var acceptedClaims = ClaimRepository.Instance.AllClaims
+                .Where(c => c.Status == "Accepted")
+                .ToList();
+
+            // setting data source for the report
+            reportDocument.SetDataSource(acceptedClaims);
+
+            //Create a Crystal Report viewer
+            CrystalReportViewer reportViewer = new CrystalReportViewer
+           {
+               ReportSource = reportDocument
+           };
+
+            // Render the report on the view
+            ViewBag.ReportViewer = reportViewer;
+
+            return View();
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
@@ -14,6 +42,10 @@ namespace Lecture_Claims_System_Web_Application.Controllers
 
             // Return the view with the accepted claims
             return View(acceptedClaims);
+        }
+
+        private class CrystalReportViewer
+        {
         }
     }
 }
